c9ca666124165f04b0259af4c781566ed712318f
poonam5248 <kaurpoonam5248@gmail.com>
2019-02-21 21:37:33 +0530

    AreaOfRectangle

---
+++ AREAOFRE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int length, breadth, area;
+
+length=52;
+breadth=48;
+
+area=length*breadth;
+printf("Area of Rectangle is: %d",area);
+getch();
+}
